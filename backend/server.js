99f8518566fc8931fcbea7a820c49636a4cb4650
angadnagrale86-prog <angadnagrale86@gmail.com>
2026-08-25 23:29:31 +0530

    Update server for deployment

--- backend/server.js
+++ backend/server.js
@@ -29,5 +29,5 @@ app.use("/api/students", studentRoutes);
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-  console.log(`Server running on http://localhost:${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
